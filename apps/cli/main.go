7bcf20ebd4257a318169e842a5efe71c2d7a807e
Vinicius Reis <luiz.vinicius73@gmail.com>
2022-06-15 14:57:06 -0300

    🚧 add urfave/cli
    Signed-off-by: Vinicius Reis <luiz.vinicius73@gmail.com>

--- apps/cli/main.go
+++ apps/cli/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/comunidade-shallom/diakonos/pkg/config"
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("ὅς ἦν ἀπό ἀρχή ὅς ἀκούω ὅς ὁράω ἡμῶν ὀφθαλμός ὅς θεάομαι καί ἡμῶν χείρ ψηλαφάω περί λόγος ζωή")
+	app := &cli.App{
+		EnableBashCompletion: true,
+		Description:          "Diakonos - Tools to speed media content development",
+		Usage:                "Diakonos CLI",
+		Copyright:            "https://github.com/comunidade-shallom",
+	}
+
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Println("Diakonos - Tools to speed media content development")
+		fmt.Println("")
+		fmt.Println(config.VersionVerbose())
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		panic(err)
+	}
 }
